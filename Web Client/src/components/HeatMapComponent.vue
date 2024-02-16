376dea7b1b47d6db8122eee549457a2566aeaf4f
SamNicklez <snicklaus@uiowa.edu>
2024-02-16 14:10:13 -0600

    Fixed heatmap to display monthly stats

--- Web Client/src/components/HeatMapComponent.vue
+++ Web Client/src/components/HeatMapComponent.vue
@@ -2,109 +2,227 @@
 export default {
     data() {
         return {
-            series: [{
-                name: 'Metric1',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            },
-            {
-                name: 'Metric2',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            },
-            {
-                name: 'Metric3',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            },
-            {
-                name: 'Metric4',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            },
-            {
-                name: 'Metric5',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            },
-            {
-                name: 'Metric6',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            },
-            {
-                name: 'Metric7',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            },
-            {
-                name: 'Metric8',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            },
-            {
-                name: 'Metric9',
-                data: this.generateData(18, {
-                    min: 0,
-                    max: 90
-                })
-            }
+            series: [
+                {
+                    name: "",
+                    data: [{
+                        x: 'Sunday',
+                        y: -1
+                    }, {
+                        x: 'Monday',
+                        y: 0
+                    }, {
+                        x: 'Tuesday',
+                        y: 50
+                    }, {
+                        x: 'Wednesday',
+                        y: 75
+                    }, {
+                        x: 'Thursday',
+                        y: 100
+                    }, {
+                        x: 'Friday',
+                        y: 29
+                    }, {
+                        x: 'Saturday',
+                        y: 13
+                    },]
+                },
+                {
+                    name: "",
+                    data: [{
+                        x: 'Sunday',
+                        y: 22
+                    }, {
+                        x: 'Monday',
+                        y: 29
+                    }, {
+                        x: 'Tuesday',
+                        y: 13
+                    }, {
+                        x: 'Wednesday',
+                        y: 32
+                    }, {
+                        x: 'Thursday',
+                        y: 22
+                    }, {
+                        x: 'Friday',
+                        y: 29
+                    }, {
+                        x: 'Saturday',
+                        y: 13
+                    },]
+                },
+                {
+                    name: "",
+                    data: [{
+                        x: 'Sunday',
+                        y: 22
+                    }, {
+                        x: 'Monday',
+                        y: 29
+                    }, {
+                        x: 'Tuesday',
+                        y: 13
+                    }, {
+                        x: 'Wednesday',
+                        y: 32
+                    }, {
+                        x: 'Thursday',
+                        y: 22
+                    }, {
+                        x: 'Friday',
+                        y: 29
+                    }, {
+                        x: 'Saturday',
+                        y: 13
+                    },]
+                },
+                {
+                    name: "",
+                    data: [{
+                        x: 'Sunday',
+                        y: 22
+                    }, {
+                        x: 'Monday',
+                        y: 29
+                    }, {
+                        x: 'Tuesday',
+                        y: 13
+                    }, {
+                        x: 'Wednesday',
+                        y: 32
+                    }, {
+                        x: 'Thursday',
+                        y: 22
+                    }, {
+                        x: 'Friday',
+                        y: 29
+                    }, {
+                        x: 'Saturday',
+                        y: 13
+                    },]
+                },
+                {
+                    name: "",
+                    data: [{
+                        x: 'Sunday',
+                        y: 22
+                    }, {
+                        x: 'Monday',
+                        y: 29
+                    }, {
+                        x: 'Tuesday',
+                        y: 13
+                    }, {
+                        x: 'Wednesday',
+                        y: 32
+                    }, {
+                        x: 'Thursday',
+                        y: 22
+                    }, {
+                        x: 'Friday',
+                        y: 29
+                    }, {
+                        x: 'Saturday',
+                        y: 13
+                    },]
+                },
+
             ],
             chartOptions: {
                 chart: {
-                    height: 350,
                     type: 'heatmap',
+                },
+                plotOptions: {
+                    heatmap: {
+                        shadeIntensity: 0.5,
+                        radius: 1,
+                        colorScale: {
+                            ranges: [
+                                {
+                                    from: -2,
+                                    to: -1,
+                                    name: 'No Data',
+                                    color: '#FFFFFF'
+                                },
+                                {
+                                    from: 0,
+                                    to: 50,
+                                    name: 'low',
+                                    color: '#00A100'
+                                },
+                                {
+                                    from: 51,
+                                    to: 75,
+                                    name: 'medium',
+                                    color: '#128FD9'
+                                },
+                                {
+                                    from: 76,
+                                    to: 100,
+                                    name: 'high',
+                                    color: '#FFB200'
+                                }]
+                        }
+                    }
                 },
                 dataLabels: {
                     enabled: false
                 },
                 colors: ["#008FFB"],
                 title: {
-                    text: 'HeatMap Chart (Single color)'
+                    text: 'HeatMap Chart (Current Month)'
+                },
+                xaxis: {
+                    type: 'category',
                 },
             },
         }
     },
+    mounted() {
+        this.generateDataForCurrentMonth()
+    },
     methods: {
-        /**
-         * Eventually this will be used to populate the heatmap with data from the servers
-         * @param {*} count 
-         * @param {*} yrange 
-         */
-        generateData(count, yrange) {
-            var i = 0;
-            var series = [];
-            while (i < count) {
-                var x = 'w' + (i + 1).toString();
-                var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-                series.push({
-                    x: x,
-                    y: y
-                });
-                i++;
+        generateDataForCurrentMonth() {
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = now.getMonth();
+
+            // Find the first and last day of the month
+            const firstDayDate = new Date(year, month, 1);
+            const lastDayDate = new Date(year, month + 1, 0); // 0th day of next month is the last day of the current month
+
+            const daysInMonth = lastDayDate.getDate();
+            const firstDayOfWeek = firstDayDate.getDay(); // Day of week the month starts on
+
+            // Calculate total slots based on starting day of the week (assuming Sunday is the start of the week)
+            const totalSlots = firstDayOfWeek + daysInMonth;
+
+            // Initialize the array with -1 for non-existing days
+            const monthArray = new Array(totalSlots).fill(-1);
+
+            // Fill the actual days with random numbers
+            for (let i = firstDayOfWeek; i < totalSlots; i++) {
+                monthArray[i] = Math.floor(Math.random() * 100) + 1;
             }
-            return series;
+            console.log(monthArray.length)
+            var row = 0
+            for(var i = 4; i > -1; i--){
+                if(i == 4){
+                    row = 1
+                }
+                else{
+                    row += 7
+                }
+                for(var j = 0; j < 7; j++){
+                    this.series[i]['data'][j].y = monthArray[row + j]
+                }
+            }
         },
     }
 }
 </script>
-
 
 <template>
     <div>
