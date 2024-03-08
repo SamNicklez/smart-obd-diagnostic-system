40713875d136c980b82bdd5a539d9406edcb0d93
SamNicklez <snicklaus@uiowa.edu>
2024-03-08 14:19:37 -0600

    Added staging route

--- Server/Flask_app.py
+++ Server/Flask_app.py
@@ -4,16 +4,6 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
 
-
-def init():
-    """
-    Initializes the server by retrieving data from the 'stage' table in Supabase.
-    """
-    response = supabase.table('stage').select("*").execute()
-    if(response.count != 0):
-        print(response)
-
-
 def calculate_mpg(MAF, Speed):
     """
     Calculates the miles per gallon (MPG) based on the Mass Air Flow (MAF) and Speed inputs.
@@ -27,17 +17,17 @@ def calculate_mpg(MAF, Speed):
     """
     # Constants
     AFR = 14.7  # Air-Fuel Ratio for gasoline
-    density_of_gasoline_kg_per_l = 0.74  # Density of gasoline in kg/L
-    grams_per_pound = 453.592  # Grams per pound
-    liters_per_gallon = 3.78541  # Liters per gallon
-    km_per_mile = 1.60934  # Kilometers per mile
+    GAS_DENSITY = 0.74  # Density of gasoline in kg/L
+    GRAMS_PER_POUND = 453.592  # Grams per pound
+    LITERS_PER_GALLON = 3.78541  # Liters per gallon
+    KM_PER_MILE = 1.60934  # Kilometers per mile
     
     # Convert MAF from grams per second to pounds per hour
-    maf_lb_per_hr = MAF * 3600 / grams_per_pound
+    maf_lb_per_hr = MAF * 3600 / GRAMS_PER_POUND
     
     # Calculate fuel consumption in gallons per hour
-    fuel_consumption_gph = (maf_lb_per_hr / AFR) / (density_of_gasoline_kg_per_l * liters_per_gallon)
-    speed_mph = Speed / km_per_mile
+    fuel_consumption_gph = (maf_lb_per_hr / AFR) / (GAS_DENSITY * LITERS_PER_GALLON)
+    speed_mph = Speed / KM_PER_MILE
     
     mpg = round(speed_mph / fuel_consumption_gph, 2)
 
@@ -50,7 +40,6 @@ supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
 CORS(app)
-init()
 
 @app.route("/")
 def home():
@@ -68,7 +57,14 @@ def login():
     except Exception as e:
         return jsonify({"Error": "Interal Server Error"}), 500
     
-
+@app.route("/stage", methods=["POST"])
+def stage():
+    try:
+        # data = request.get_json()
+        # response = supabase.table('stage').insert(data).execute()
+        return jsonify({"Test": "GOOD"}), 200
+    except Exception as e:
+        return jsonify({"Error": "Interal Server Error"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
