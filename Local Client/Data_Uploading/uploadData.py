55d8fe60a65eb6a5e030babb666e9f34c4e031e1
ColeArduser <carduser@uiowa.edu>
2024-03-28 13:48:05 -0500

    data upload

--- Local Client/Data_Uploading/uploadData.py
+++ Local Client/Data_Uploading/uploadData.py
@@ -1,45 +1,36 @@
+import socket
+from datetime import datetime
+
 import mysql.connector
 import requests
-from datetime import datetime
-import socket
 
 # Database connection configuration
 db_config = {
-    'user': 'sloecke',
+    'user': 'test',
     'password': 'password',
     'host': 'localhost',
+    'port': '3307',
     'database': 'obd'
 }
 
-# Flask server configuration
-server_url = 'http://172.17.100.225:5000/upload'
 
-def check_internet_connection(host="8.8.8.8", port=53, timeout=3):
-    """
-    Check if the Raspberry Pi is connected to the internet.
-    
-    Attempts to establish a socket connection with Google's DNS server.
-    If successful, it implies an active internet connection.
-    
-    :param host: The host IP to connect to for checking the internet connection.
-    :param port: The port number to use for the connection.
-    :param timeout: Connection timeout in seconds.
-    :return: True if the internet connection is available, False otherwise.
-    """
+def get_token():
     try:
-        # Use the socket to attempt to connect to the host
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        return True
-    except socket.error as ex:
-        print(f"No internet connection: {ex}")
-        return False
+        response = requests.post(
+            url="https://senior-design-final-project.onrender.com/login",
+            json={"username": "username", "password": "password"},
+            headers={"Content-Type": "application/json"}
+        )
+        if response.status_code == 200:
+            return response.json().get("token")
+        else:
+            print(f"Failed to get token: {response.status_code}")
+    except Exception as e:
+        print(f"Error getting token: {e}")
+    return None
+
 
 def fetch_data_from_database():
-    """
-    Fetch data from the MySQL database.
-    :return: A list of dictionaries with the data, where datetime objects are converted to strings.
-    """
     data = []
     try:
         db_conn = mysql.connector.connect(**db_config)
@@ -53,22 +44,25 @@ def fetch_data_from_database():
                 if isinstance(value, datetime):
                     row[key] = value.isoformat()
             data.append(row)
-        
+
         db_cursor.close()
         db_conn.close()
     except mysql.connector.Error as err:
         print(f"Failed to fetch data from database: {err}")
-    
+
     return data
 
 
-def send_data_to_server(data):
-    """
-    Send data to the Flask server via POST request.
-    :param data: The data to send, as a list of dictionaries.
-    """
+def send_data_to_server(data, token):
     try:
-        response = requests.post(server_url, json=data)
+        response = requests.post(
+            url="https://senior-design-final-project.onrender.com/postOBDData",
+            json=data,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token,
+            }
+        )
         if response.status_code == 200:
             print("Data sent successfully to the server.")
         else:
@@ -76,13 +70,21 @@ def send_data_to_server(data):
     except Exception as e:
         print(f"Error sending data to server: {e}")
 
+
+def upload_data():
+    token = get_token()
+    print(f"Token: {token}")
+    data = fetch_data_from_database()
+    print(f"Data: {data}")
+    if data:
+        send_data_to_server(data, token)
+    else:
+        print("No data to send.")
+
+
 def main():
-    if check_internet_connection():
-        data = fetch_data_from_database()
-        if data:
-            send_data_to_server(data)
-        else:
-            print("No data to send.")
+    upload_data()
+
 
 if __name__ == "__main__":
     main()
