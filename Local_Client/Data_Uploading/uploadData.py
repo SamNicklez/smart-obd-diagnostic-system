9a5b23e49957b5a6ae09a5d25934a75e3434d194
ColeArduser <carduser@uiowa.edu>
2024-04-10 19:09:45 -0500

    changed data upload

---
+++ Local_Client/Data_Uploading/uploadData.py
@@ -0,0 +1,141 @@
+from datetime import datetime, timedelta
+import random
+
+import mysql.connector
+from Local_Client.PrintInColor import printc
+import requests
+
+# Database connection configuration
+db_config = {
+    'user': 'test',
+    'password': 'password',
+    'host': 'localhost',
+    'port': '3307',
+    'database': 'obd'
+}
+
+
+def get_token():
+    try:
+        response = requests.post(
+            # url="https://senior-design-final-project.onrender.com/login",
+            url="http://127.0.0.1:5000/login",
+            json={"username": "username", "password": "password"},
+            headers={"Content-Type": "application/json"}
+        )
+        if response.status_code == 200:
+            return response.json().get("token")
+        else:
+            print(f"Failed to get token: {response.status_code}")
+    except Exception as e:
+        print(f"Error getting token: {e}")
+    return None
+
+
+def fetch_and_delete_data_from_database():
+    data = []
+    try:
+        db_conn = mysql.connector.connect(**db_config)
+        db_cursor = db_conn.cursor(dictionary=True)
+        db_conn.start_transaction()
+
+        select_query = "SELECT * FROM VehicleData"
+        db_cursor.execute(select_query)
+        for row in db_cursor:
+            for key, value in row.items():
+                if isinstance(value, datetime):
+                    row[key] = value.isoformat()
+            data.append(row)
+
+        delete_query = "DELETE FROM VehicleData"
+        db_cursor.execute(delete_query)
+
+        db_conn.commit()
+
+        db_cursor.close()
+        db_conn.close()
+    except mysql.connector.Error as err:
+        print(f"Failed to fetch data from database: {err}")
+
+    return data
+
+
+def send_data_to_server(data, token):
+
+    try:
+        response = requests.post(
+            # url="https://senior-design-final-project.onrender.com/stage",
+            url="http://127.0.0.1:5000/stage",
+            json=data,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token,
+            }
+        )
+        if response.status_code == 200:
+            print("Data sent successfully to the server.")
+        else:
+            print(f"Failed to send data to server: {response.status_code}")
+    except Exception as e:
+        print(f"Error sending data to server: {e}")
+
+
+def format_data(data):
+    # mock_timestamp = datetime.now()
+    mock_timestamp = datetime.now() - timedelta(days=18)
+    return_data = []
+    list_of_end_points = [("41.683430", "-91.562100"), ("41.703860", "-91.619360"),
+                          ("41.646198", "-91.551003"), ("41.665890", "-91.471680"),
+                          ("41.725360", "-91.519960"), ("41.698840", "-91.565990"),
+                          ("41.697731", "-91.525510"), ("41.721920", "-91.525510"),
+                          ("41.663880", "-91.530370"), ("41.659290", "-91.505840"),
+                          ("41.651409", "-91.500900"), ("41.646969", "-91.532738")]
+    random_end_point = random.choice(list_of_end_points)
+    for i, row in enumerate(data):
+        formatted_row = {
+            "timestamp": (mock_timestamp + timedelta(seconds=row['RUN_TIME'])).isoformat(),
+            "airflow_rate": row['MAF'],
+            "speed": row['SPEED'],
+            "relative_throttle_pos": row['RELATIVE_THROTTLE_POS'],
+            "distance_w_mil": row['DISTANCE_W_MIL'],
+            "runtime": row['RUN_TIME'],
+            "commanded_egr": row['COMMANDED_EGR'],
+            "time_since_dtc_cleared": row['TIME_SINCE_DTC_CLEARED'],
+            "runtime_mil": row['RUN_TIME_MIL'],
+            "intake_pressure": row['INTAKE_PRESSURE'],
+            "coolant_temp": row['COOLANT_TEMP'],
+            "oil_temp": row['OIL_TEMP'],
+            "barometric_pressure": row['BAROMETRIC_PRESSURE'],
+            "rpm": row['RPM'],
+            "pids_b": row['PIDS_B'],
+            "intake_temp": row['INTAKE_TEMP'],
+            "voltage": row['CONTROL_MODULE_VOLTAGE'],
+            "absolute_load": row['ABSOLUTE_LOAD'],
+            "engine_load": row['ENGINE_LOAD'],
+            "dtc": row['GET_DTC'] if row['GET_DTC'] else "None",
+            "current_dtc": row['GET_CURRENT_DTC'] if row['GET_CURRENT_DTC'] else "None",
+            "latitude": "41.658200" if i != len(data) - 1 else random_end_point[0],
+            "longitude": "-91.533460" if i != len(data) - 1 else random_end_point[1],
+        }
+        return_data.append(formatted_row)
+    return return_data
+
+
+def upload_data():
+    token = get_token()
+    print(f"Token: {token}")
+    data = fetch_and_delete_data_from_database()
+    data = format_data(data)
+    printc(f"Data: {data}")
+    if data:
+        send_data_to_server(data, token)
+    else:
+        print("No data to send.")
+
+
+def main():
+    upload_data()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Local_Client/Data_Uploading/wifiConnection.py
@@ -0,0 +1,31 @@
+import socket
+
+
+def check_internet_connection(host="8.8.8.8", port=53, timeout=3):
+    """
+    Check if the Raspberry Pi is connected to the internet.
+    
+    Attempts to establish a socket connection with Google's DNS server.
+    If successful, it implies an active internet connection.
+    
+    :param host: The host IP to connect to for checking the internet connection.
+    :param port: The port number to use for the connection.
+    :param timeout: Connection timeout in seconds.
+    :return: True if the internet connection is available, False otherwise.
+    """
+    try:
+        # Use the socket to attempt to connect to the host
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(f"No internet connection: {ex}")
+        return False
+
+
+def main():
+    print("Waiting for internet connection...")
+
+
+if __name__ == "__main__":
+    main()
