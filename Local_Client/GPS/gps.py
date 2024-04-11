1fe41727b650d93757316190afb0a2f76c20009f
ColeArduser <carduser@uiowa.edu>
2024-04-11 10:51:44 -0500

    gps

--- Local_Client/GPS/gps.py
+++ Local_Client/GPS/gps.py
@@ -1,41 +1,42 @@
 import io
-import serial
-import sys
-import logging
+
 import pynmea2
+import serial
+
 
 class GPS:
-        def __init__(self):
-                self.ser = serial.Serial(
-                        port = '/dev/serial0',
-                        baudrate = 9600,
-                        parity = serial.PARITY_NONE,
-                        stopbits = serial.STOPBITS_ONE,
-                        bytesize = serial.EIGHTBITS,
-                        timeout = 1
-                ) 
-                self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser))
+    def __init__(self):
+        self.ser = serial.Serial(
+            port='/dev/serial0',
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
+        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser))
 
+    def get_gps_data(self):
+        while True:
+            line = self.sio.readline()
+            msg = pynmea2.parse(line)
 
-        def get_gps_data(self):
-                while True:
-                        line = self.sio.readline()
-                        msg = pynmea2.parse(line)
+            if type(msg) == pynmea2.types.talker.RMC:
+                if msg.lat and msg.lon:
+                    return msg.lat, msg.lon
+                else:
+                    return None, None
 
-                        if type(msg) == pynmea2.types.talker.RMC:
-                                if msg.lat and msg.lon:
-                                        return msg.lat, msg.lon
-                                else:
-                                        return None, None
 
 def main():
-        gps = GPS()
-        while True:
-                lat, lon = gps.get_gps_data()
-                if lat and lon:
-                        print("Lat: " + lat + "\nLon: " + lon)
-                else:
-                        print("Lat: None\nLon: None")
+    gps = GPS()
+    while True:
+        lat, lon = gps.get_gps_data()
+        if lat and lon:
+            print("Lat: " + lat + "\nLon: " + lon)
+        else:
+            print("Lat: None\nLon: None")
+
 
 if __name__ == "__main__":
-        main()
+    main()
