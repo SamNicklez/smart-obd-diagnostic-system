40decc43aaf134ca8d8f42f3fa755ac638bc0e50
Luke Farmer <ljfarmer@uiowa.edu>
2024-04-16 16:10:39 -0500

    Fixed MDRaisedButton

--- Local_Client/GUI/FlashingButton.py
+++ Local_Client/GUI/FlashingButton.py
@@ -1,31 +1,45 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.popup import Popup
 from kivy.clock import Clock
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.dialog import MDDialog
 
 # Define the FlashingButton class
-class FlashingButton(Button):
+class FlashingButton(MDRaisedButton):
     def __init__(self, **kwargs):
         super(FlashingButton, self).__init__(**kwargs)
         self.flash_state = False
-        self.condition = True  # You'll need to set this based on your specific condition
-        self.flash_event = Clock.schedule_interval(self.update_flash, 1)
+        self.condition = True  # This will start the button flashing immediately
+        self.flash_event = Clock.schedule_interval(self.update_flash, 1)  # Flash every second
+        self.dialog = None  # Initialize dialog attribute
 
     def update_flash(self, dt):
         if self.condition:
             self.flash_state = not self.flash_state
             if self.flash_state:
-                self.background_color = (1, 0, 0, 1)  # Red color
+                self.md_bg_color = (1, 0, 0, 1)  # Red color
             else:
-                self.background_color = (1, 1, 1, 1)  # White color
+                self.md_bg_color = (1, 1, 1, 1)  # White color
         else:
-            self.background_color = (1, 1, 1, 1)
+            self.md_bg_color = self.theme_cls.primary_color  # Reset to default theme color
 
     def on_press(self):
         # Display a Popup on button press
-        content = BoxLayout(orientation='vertical')
-        close_btn = Button(text='Close')
-        content.add_widget(close_btn)
-        popup = Popup(title='Menu', content=content, size_hint=(None, None), size=(400, 400))
-        close_btn.bind(on_press=popup.dismiss)
-        popup.open()
+        self.show_alert_dialog()
+
+    def show_alert_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Menu",
+                type="custom",
+                content_cls=BoxLayout(orientation='vertical'),
+                buttons=[
+                    MDRaisedButton(
+                        text="Close",
+                        on_release=self.close_dialog
+                    ),
+                ],
+            )
+        self.dialog.open()
+
+    def close_dialog(self, *args):
+        self.dialog.dismiss()
